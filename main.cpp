ead220360f6d1b2d7e7b69a1856d59505700f2bd
0doxes0 <0doxes0@gmail.com>
2024-11-07 22:21:25 -0700

    Add naming, a few tool slots and small changes

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-
     QString brightStyleSheet = "QWidget {"
                              "  background-color: #EEEEEE;"
                              "  color: #1E1E1E;"
@@ -22,24 +21,8 @@ int main(int argc, char *argv[])
                              "}"
                              "QToolButton:pressed {"
                              "  background-color: #EEEEEE;"
-                             "}";
-
-    QString darkStyleSheet = "QWidget {"
-                             "  background-color: #1E1E1E;"
-                             "  color: #E0E0E0;"
                              "}"
-                             "QToolButton {"
-                             "  background-color: #3C3C3C;"
-                             "  border-radius: 5px;"
-                             "  padding: 5px;"
-                             "  color: #eee;"
-                             "}"
-                             "QToolButton:hover {"
-                             "  background-color: #8C8C8C;"
-                             "}"
-                             "QToolButton:pressed {"
-                             "  background-color: #282828;"
-                             "}";
+                            ;
 
     a.setStyleSheet(brightStyleSheet);
 
