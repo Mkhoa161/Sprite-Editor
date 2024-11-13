89cf32ae232bdf162fe67dff1bbc190617e7a3e1
Duke Nguyen <u1445624@utah.edu>
2024-11-13 13:38:52 -0700

    Refactor - Split paintPixels method into smaller helper methods

--- canvas.h
+++ canvas.h
@@ -1,3 +1,16 @@
+/*
+    Authors: Zhuyi Bu, Zhenzhi Liu, Justin Melore, Maxwell Rodgers, Duke Nguyen, Minh Khoa Ngo
+    Github usernames: 1144761429, 0doxes0, JustinMelore, maxdotr, duke7012, Mkhoa161
+    Date: November 12th, 2024
+    Class: CS3505, Fall 2024
+    Assignment - A8: Sprite Editor Implementation
+
+    The Canvas class is a custom QWidget used within the Sprite Editor application, enabling users to paint, erase,
+    and manipulate pixels within a designated drawing area. As the primary editing surface in the view,
+    Canvas allows for pixel specific changes, supporting various tools, shapes, and colors
+    to create and modify individual frames in the animation sequence.
+*/
+
 #ifndef CANVAS_H
 #define CANVAS_H
 
@@ -64,6 +77,7 @@ private:
 
     int sideLength;
     int pixelSize;
+    int canvasSize;
 
     QPoint mousePixelPos;
     bool isPressingMouse;
@@ -93,6 +107,15 @@ private:
     void setShapeMode(bool enabled);
     void moveAndDisplayPixels(QColor color);
     void redrawShape();
+
+    void brushPainting(QColor color);
+    void eraserPainting(QColor color);
+    void squarePainting(QColor color);
+    void squareFilledPainting(QColor color);
+    void circlePainting(QColor color);
+    void circleFilledPainting(QColor color);
+    void trianglePainting(QColor color);
+    void triangleFilledPainting(QColor color);
 
     QRect getPixelRect(QPoint pixelPos);
 
